cf7470c1f15e5af8280274e161790e05db38c154
novanjanis322 <novangfc@gmail.com>
2023-03-29 12:40:15 +0700

    login guru, seeder role guru

--- app/Models/Guru.php
+++ app/Models/Guru.php
@@ -2,14 +2,53 @@
 
 namespace App\Models;
 
+use App\Models\Order;
+use App\Models\Course;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Guru extends Model
+class Guru extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
-
+        'name',
+        'email',
+        'username',
+        'password'
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function course () {
+        return $this->hasMany(Course::class);
+    }
+    public function order () {
+        return $this->hasMany(Order::class);
+    }
 }
